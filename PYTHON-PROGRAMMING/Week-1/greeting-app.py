f411025d575325b4984b23de9fb1b7b9cd5fa4c6
MrNarman <godricknarman21@gmail.com>
2025-07-31 22:33:25 +0300

    Completed the personaliszed greeting app

---
+++ PYTHON-PROGRAMMING/Week-1/greeting-app.py
@@ -0,0 +1,6 @@
+user_name = input("Please enter your name: ")
+favourite_color = input("Please enter your favorite color: ")
+
+print(f"Hello, {user_name}! Your favorite color is {favourite_color}.")
+
+
