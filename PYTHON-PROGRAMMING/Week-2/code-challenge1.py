ec5086f1fa3603b079c2c16f9eea2a4b79a9453a
MrNarman <godricknarman21@gmail.com>
2025-08-12 22:40:43 +0300

    Python week2 code challenge 1

---
+++ PYTHON-PROGRAMMING/Week-2/code-challenge1.py
@@ -0,0 +1,7 @@
+numbers = []
+for i in range(3):
+    user_number = input("Enter a number: ")
+    numbers.append(int(user_number))
+
+print("The sum of the numbers you entered is:", sum(numbers))
+    
